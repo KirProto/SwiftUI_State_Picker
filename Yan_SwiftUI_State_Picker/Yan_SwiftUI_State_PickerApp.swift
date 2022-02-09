10052a82e0a47077619098dbed9361751632e761
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 10:24:08 +0500

    Initial Commit

---
+++ Yan_SwiftUI_State_Picker/Yan_SwiftUI_State_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_State_PickerApp.swift
+//  Yan_SwiftUI_State_Picker
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_State_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
